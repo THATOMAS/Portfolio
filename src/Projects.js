a128050064e8d1881d92059ab2d2d09ca1cc36c5
THATOMAS <chachumashori@gmail.com>
2022-05-07 13:25:32 +0200

    Mapped through the projects array and created on click button filter the array . All that is left is to place coresponding backgrounds.

---
+++ src/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Projects = [
+  { id: 1, img: "./assets/Portfolio/menu.png", category: "React",cName:'single-project' },
+  { id: 2, img: "./assets/Portfolio/reviews.png", category: "React",cName:'single-project' },
+  { id: 3, img: "./assets/Portfolio/slider.png", category: "React" ,cName:'single-project'},
+  { id: 4, img: "./assets/Portfolio/tabs.png", category: "React",cName:'single-project' },
+  { id: 5, img: "./assets/Portfolio/tours.png", category: "React" ,cName:'single-project'},
+  {id:6,img:'./assets/Portfolio/tracker.png',category:'Mern',cName:'single-project'
+    }];
+
+export default Projects;
+
--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+
+import React ,{useState}from 'react'
 import Buttons from '../PortfolioButtons';
+import Projects from '../Projects'
 const Portfolio = ()=>{
+
+    const [projects,setProjects] = useState(Projects)
+
+    const filterProjects = (category)=>{
+        const newProjects = Projects.filter((project)=> project.category === category )
+        setProjects(newProjects)
+        if(category === 'All'){
+            setProjects(Projects)
+            return
+        }
+        
+   
+    }
+
     return(
         <article id='portfolio-page' className='single-page'>
             <div id="portfolio-page-center">
@@ -8,16 +24,33 @@ const Portfolio = ()=>{
             <div id='portfolio-buttons'>
                 {Buttons.map((button)=>{
                     return(
-                        <div key={button.id} className={button.cName}>
+                        <button type='button' key={button.id} className={button.cName} onClick={()=>filterProjects(button.name)}>
                             <p>{button.name}</p>
 
-                        </div>
+                        </button>
                     )
                 })}
             </div>
             <div id='underline'></div>
             <div id='portfolio-projects'>
+                {projects.map((project)=>{
 
+                    return (
+                        <div key={project.id} 
+                        className={project.cName} 
+                        style={{height:'50vh',
+                        width:'32vw',
+                        margin:'1em',
+                        background:`red`,
+                        borderRadius:'0.5em'
+                        
+                        
+                        
+                        }}>
+
+                        </div>
+                    )
+                })}
             </div>
             </div>
         </article>
